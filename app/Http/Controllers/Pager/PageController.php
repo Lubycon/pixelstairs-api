0c9afecdf3c8458925185e14fb63a6d76f330abb
daniel_zepp <bboyzepot@gmail.com>
2017-01-12 10:59:43 +0900

    pager refactorting

--- app/Http/Controllers/Pager/PageController.php
+++ app/Http/Controllers/Pager/PageController.php
@@ -20,26 +20,19 @@ use Log;
 class PageController extends Controller
 {
     private $model;
-    private $setModel = null;
+    private $filteredModel = null;
     private $query;
-    private $categoryName;
     private $firstFageNumber = 0;
     private $maxSize = 50;
     private $defaultSize = 20;
-    private $searchAllUser = false;
-    private $searchAllPost = false;
     private $sortDefault = 0; // 0 = recent, default result
     private $sort;
 
-    private $setPage;
+    private $pageNumber;
     private $pageSize;
-    private $searchUser;
-    private $searchUserName = 'id';
-    private $searchPostName = 'id';
-    private $searchMarketCategoryId = 'market_category_id';
     private $sortOption;
+    private $filterOptions = [];
 
-    private $userModelFunctionName = 'user';
     private $withUserModel;
     public $paginator;
     public $totalCount;
@@ -57,23 +50,14 @@ class PageController extends Controller
         $this->query = $query;
         $this->sort = (object)array('option' => 'created_at','direction' => 'desc');
 
-//        $this->categoryName = $this->getSectionName($section);
         $this->setModel($section);
-        $this->setPageRange();
-        $this->setModelFilter();
+        $this->pageNumber = $this->setPageNumber();
+        $this->pageSize = $this->setPageSize();
+        $this->setSort();
+        $this->setFilter();
+        $this->modelFiltering();
         $this->bindData();
     }
-
-//    private function getSectionName($section){
-////         if($section == 'comment'){
-////             return 'comment';
-////         }else{
-////             return Board::where('name','=',$section)->value('group');
-////         }
-//        switch ($section){
-//
-//        }
-//    }
 
     private function setModel($section){
         switch($section){
@@ -90,7 +74,6 @@ class PageController extends Controller
         // if( isset($this->query['sort']) && $this->query['sort'] > 4 ){
         //     $this->query['sort'] = 1;
         // }
-        // $this->searchUserName = 'user_id';
         $this->query['sort'] = 1;
         return;
     }
@@ -107,12 +90,26 @@ class PageController extends Controller
         return;
     }
 
-    private function setPageRange(){
-        $this->setPage = isset($this->query['pageIndex']) ? $this->query['pageIndex'] : $this->firstFageNumber;
-        $this->pageSize = isset($this->query['pageSize']) && $this->query['pageSize'] <= $this->maxSize ? $this->query['pageSize'] : $this->defaultSize;
-        // $this->searchPost = isset($this->query['boardId']) ? $this->query['boardId'] : $this->searchAllPost;
-        // $this->searchUser = isset($this->query['userId']) ? $this->query['userId'] : $this->searchAllUser;
-//        $this->searchMarketCategory = isset($this->query['userId']) ? $this->query['userId'] : $this->searchAllUser;
+    private function setPageNumber(){
+        return isset($this->query['pageIndex']) ? $this->query['pageIndex'] : $this->firstFageNumber;
+    }
+    private function setPageSize(){
+        return isset($this->query['pageSize']) && $this->query['pageSize'] <= $this->maxSize ? $this->query['pageSize'] : $this->defaultSize;
+    }
+
+    private function setFilter(){
+
+        // in sector filter
+        if( isset($this->query['marketCategoryId']) ){
+            $this->filterOptions[] = array(
+                "columnName" => 'market_category_id',
+                "value" => $this->query['marketCategoryId'] == 'null' ? NULL : $this->query['marketCategoryId'] ,
+            );
+        }
+        // in sector filter
+    }
+
+    private function setSort(){
         $this->sortOption = isset($this->query['sort']) ? $this->query['sort'] : $this->sortDefault;
         switch($this->sortOption){
             case 1 : break; //recent
@@ -123,46 +120,38 @@ class PageController extends Controller
         }
     }
 
-    private function setModelFilter(){
-        // if($this->searchUser){ //target users search
-        // Log::debug($this->searchUserName.'='.$this->searchUser);
-        //     $this->setModel = $this->model->where($this->searchUserName,'=',$this->searchUser);
-        // }
-        // if($this->searchPost){ // target posts search
-        //     $this->setModel = $this->setModel == null
-        //     ?$this->model->where($this->searchPostName,'=',$this->searchPost)
-        //     :$this->setModel->where($this->searchPostName,'=',$this->searchPost);
-        // }
-//         if($this->searchPost){ // target posts search
-//             $this->setModel = $this->setModel == null
-//             ?$this->model->where($this->searchPostName,'=',$this->searchPost)
-//             :$this->setModel->where($this->searchPostName,'=',$this->searchPost);
-//         }
-        if($this->setModel == null){ //default search
-            $this->initModel();
+    private function modelFiltering(){
+        if( $this->hasFilter() ){
+            foreach( $this->filterOptions as $key => $value ){
+                $this->filteredModel = $this->model->where($value['columnName'],'=',$value['value']);
+            }
         }
+
+        //none filtered model
+        if($this->filteredModel == null) $this->initModel();
+    }
+
+    private function hasFilter(){
+        return count($this->filterOptions);
     }
 
     private function initModel(){
-        $this->setModel = $this->model;
+        $this->filteredModel = $this->model;
     }
 
     private function bindData(){
-        $this->withUserModel = $this->setModel;
-        $this->paginator = $this->withUserModel->
+//        $this->withUserModel = $this->setModel;
+        $this->paginator = $this->filteredModel->
             orderBy($this->sort->option,$this->sort->direction)->
-            paginate($this->pageSize, ['*'], 'page', $this->setPage);
-            // Log::debug('pagnator', [DB::getQueryLog()]);
+            paginate($this->pageSize, ['*'], 'page', $this->pageNumber);
+//             Log::debug('pagnator', [DB::getQueryLog()]);
         $this->totalCount = $this->paginator->total();
         $this->currentPage = $this->paginator->currentPage();
         $this->collection = $this->paginator->getCollection();
-
-        if($this->collection->isEmpty()){
-            return response()->success();
-        }
     }
 
     public function getCollection(){
+//        return [];
         return $this->collection;
     }
 }
