7d9ebcdf41fbcd3d4e53fb1047571328fd70d8ae
daniel_zepp <bboyzepot@gmail.com>
2017-01-13 14:33:56 +0900

    swagger php install, default setting

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * @SWG\Swagger(
+ *     schemes={"http"},
+ *     host="dev-api@mittycompany.com",
+ *     basePath="/v1",
+ *     @SWG\Info(
+ *         version="1.0.0",
+ *         title="Walter Mitty Dev API Document",
+ *         termsOfService="",
+ *         @SWG\Contact(
+ *             email="daniel@mittycompany.com"
+ *         ),
+ *         @SWG\License(
+ *             name="Private License"
+ *         )
+ *     ),
+ *     @SWG\ExternalDocumentation(
+ *         description="Find out more about my website",
+ *         url="http://www.mittycompany.com"
+ *     )
+ * )
+ */
+
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
