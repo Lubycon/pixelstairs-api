75e38d69e7a17fa6ebc4dc2b1e64eba1c2f0c4b4
daniel_zepp <bboyzepot@gmail.com>
2017-02-02 19:01:28 +0900

    crawler making

--- app/Crawlers/CoupangCrawler.php
+++ app/Crawlers/CoupangCrawler.php
@@ -36,48 +36,118 @@ class CoupangCrawler
         $this->vendor_id = $idInfo->data_vendor_item_id;
 
         $basicProductInfo = $this->basicProductInfo(); // product basic infomation
+        $basicProductInfWithStock = $this->basicProductInfWithStock(); // product basic infomation with stock
         $vendorProductInfo = $this->vendorProductInfo(); // product require info + product detail images
-
+        $optionData = $this->optionData(); // product require info + product detail images
+        $productAtf = $this->productAtf(); // product title
 
 
         $this->result = [
-            "basicInfo" => $basicProductInfo,
-            "vendorInfo" => [
-                "requireInfo" => $vendorProductInfo['requireInfo'],
+            "id" => $this->product_id,
+            "name" => $productAtf['name'],
+            "category" => [],
+            "priceInfo" => [
+                "price" => $basicProductInfWithStock['price'],
+                "lowestPrice" => 0
             ],
-            "detailImage" => $vendorProductInfo['detailImage']
+            "deliveryPrice" => $basicProductInfWithStock['deliveryPrice'],
+            "thumbnailUrl" => $optionData['options'][0]["thumbnailUrl"],
+            "optionKey" => $optionData['optionKey'],
+            "options" => $optionData['options'],
+            "original" => [
+                "name" => $productAtf['name'],
+                "basicInfo" => $basicProductInfo,
+                "requireInfo" => $vendorProductInfo['requireInfo'],
+                "detailImage" => $vendorProductInfo['detailImage'],
+                "basicProductInfWithStock" => $basicProductInfWithStock,
+                "options" => $optionData['options'],
+                "optionKey" => $optionData['optionKey'],
+            ],
         ];
     }
 
+    protected  function productAtf(){
+        $requestUrl = "https://www.coupang.com/vp/products/$this->product_id/product-atf?itemId=$this->item_id&vendorItemId=$this->vendor_id";
+        $dom = $this->getDomResult($requestUrl);
+        return [
+            "name" => $this->getText($dom,'.prod-buy-header__title'),
+        ];
+    }
     protected function basicProductInfo(){
         $requestUrl = "https://www.coupang.com/vp/products/$this->product_id/vendor-items/$this->vendor_id/selling-infos?itemId=$this->item_id";
         $this->snoopy->fetchtext($requestUrl);
-        $source = $this->snoopy->results;
-        return $source;
+        $dom = $this->snoopy->results;
+        return $dom;
     }
-
+    protected function basicProductInfWithStock(){
+        $requestUrl = "https://www.coupang.com/vp/products/$this->product_id/vendor-items/$this->vendor_id/sale-infos/sdp";
+        $dom = $this->getDomResult($requestUrl);
+        return [
+            "isSoldOut" => $this->getElementAttribute($dom,'.prod-value-holder','data-is-sold-out'),
+            "deliveryPrice" => $this->getElementAttribute($dom,'.prod-value-holder','data-shipping-fee') == ""
+                ? 0
+                : $this->getElementAttribute($dom,'.prod-value-holder','data-shipping-fee'),
+            "price" => $this->getElementAttribute($dom,'.prod-value-holder','data-sale-price'),
+            "totalStock" => $this->getElementAttribute($dom,'.prod-value-holder','data-stock-quantity'),
+            "maxBuyAble" => $this->getText($dom,'.prod-buyable-quantity'),
+        ];
+    }
     protected function vendorProductInfo(){
         $requestUrl = "https://www.coupang.com/vp/products/$this->product_id/vendor-items/$this->vendor_id?isFixedVendorItem=true&type=sdp";
-        $this->snoopy->fetch($requestUrl);
-        $source = $this->snoopy->results;
-        $source = str_replace(array("\r\n","\r","\n"),'',$source);
-        $dom = $this->dom->load($source);
+        $dom = $this->getDomResult($requestUrl);
         $requireInfo = $this->getMergeText($dom,'.prod-item-attr-name');
         $optionsImg = $this->getImageSrc($dom,'.lazy-img','data-src');
-
-
         return [
             "requireInfo" => $requireInfo,
             "detailImage" => $optionsImg,
         ];
     }
+    protected function optionData(){
+        $requestUrl = "https://www.coupang.com/vp/products/$this->product_id/loadOptions?itemId=$this->item_id&vendorItemId=$this->vendor_id&&noAttribute=false";
+        $dom = $this->getDomResult($requestUrl);
+        $optionKey[] = $this->getText($dom,'.prod-option-name__button');
+        $options = $this->getElement($dom,'.prod-option-select__item');
+        $optionResult = [];
+        foreach( $options as $value ){
+            $optionResult[] = [
+                "name" => $value->getAttribute('data-option-title'),
+                "thumbnailUrl" => $value->getAttribute('data-option-img-src'),
+            ];
+        }
 
+        return [
+            "optionKey" => $optionKey,
+            "options" => $optionResult,
+        ];
+    }
+
+
+
+
+    public function getDomResult($requestUrl){
+        $this->snoopy->fetch($requestUrl);
+        $source = $this->snoopy->results;
+        $source = str_replace(array("\r\n","\r","\n"),'',$source);
+        return $this->dom->load($source);
+    }
+    public function getText($dom,$findWord){
+        $requireDom = $dom->find($findWord)->text;
+        return $requireDom;
+    }
     public function getMergeText($dom,$findWord){
         $requireDom = $dom->find($findWord);
         $result = '';
         foreach ($requireDom as $value)
         {$result .= $value->text."\n";}
         return $result;
+    }
+    public function getElement($dom,$findAttr){
+        $requireDom = $dom->find($findAttr);
+        return $requireDom;
+    }
+    public function getElementAttribute($dom,$findAttr,$chooseAttr){
+        $requireDom = $dom->find($findAttr)->getAttribute($chooseAttr);
+        return $requireDom;
     }
     public function getImageSrc($dom,$findAttr,$chooseAttr){
         $requireDom = $dom->find($findAttr);
