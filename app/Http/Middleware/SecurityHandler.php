94a9b87ea371a9ea5209f53de6b950a8bd94542d
daniel_zepp <bboyzepot@gmail.com>
2017-02-07 13:16:54 +0900

    security handler edit

--- app/Http/Middleware/SecurityHandler.php
+++ app/Http/Middleware/SecurityHandler.php
@@ -33,16 +33,16 @@ class SecurityHandler
 		}
 
          if( !$this->isOptionMethod($request) ){
-             if ( $this->devPassKey($request) !== $this->devPassValue() ) {
-                 Abort::Error('0043','Check Dev Pass');
-             }
+//             if ( $this->devPassKey($request) !== $this->devPassValue() ) {
+//                 Abort::Error('0043','Check Dev Pass');
+//             }
              if ( !$this->apiUrlVersionCheck($request) || !$this->apiVersionCheck($request) ) {
                  Abort::Error('0073','Check Current API version');
              }
              if ( $this->isProvision() ) {
-                 if ( !$this->checkProvisionFront($request) ) {
-                     Abort::Error('0073');
-                 }
+//                 if ( !$this->checkProvisionFront($request) ) {
+//                     Abort::Error('0073');
+//                 }
              }
              if ( !$this->requiredHeaderCheck($request) ) {
                  Abort::Error('0047');
