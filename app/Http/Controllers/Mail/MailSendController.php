ab5cdb3988461df7238ff3b39b291325664d32dd
Evan Moon <bboydart91@gmail.com>
2017-06-10 16:48:38 +0900

    PXS-96: SignupReminderMailSendJob -> SignupMailSendJob in MailSendController.php * expire date will be updated * token will be updated

--- app/Http/Controllers/Mail/MailSendController.php
+++ app/Http/Controllers/Mail/MailSendController.php
@@ -16,7 +16,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 //Jobs
-use App\Jobs\Mails\SignupReminderMailSendJob;
+use App\Jobs\Mails\SignupMailSendJob;
 use App\Jobs\Mails\PasswordReMinderSendMailJob;
 
 // Request
@@ -31,9 +31,9 @@ class MailSendController extends Controller
         $this->user = User::class;
     }
 
-    public function resendSignup(MailRemindSignupRequest $request){
+    public function sendSignup(MailRemindSignupRequest $request){
         $this->user = User::getAccessUser();
-        $this->dispatch(new SignupReminderMailSendJob($this->user));
+        $this->dispatch(new SignupMailSendJob($this->user));
         return response()->success();
     }
 }
