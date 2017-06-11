3f49ae38dee2249273548956bf9e716d1aa6031e
Evan Moon <bboydart91@gmail.com>
2017-06-11 16:32:34 +0900

    origin test

--- resources/views/emails/signup.blade.php
+++ resources/views/emails/signup.blade.php
@@ -61,7 +61,7 @@
             </div>
         </div>
         <div class="card button-wrapper">
-            <a class="btn" href="{{ app('request')->headers->get('Origin').'/certs/signup/landing/'.$token }}" style="
+            <a class="btn" href="{{ app('request')->headers->get('origin').'/certs/signup/landing/'.$token }}" style="
                 display: inline-block;
                 width: 100%;
                 height: 50px;
