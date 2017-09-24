7c19a2ea3a9e20675594d0c4cada6c84603a18ff
daniel_zepp <bboyzepot@gmail.com>
2017-09-24 17:51:30 +0900

    Max-length Error fix

--- app/Http/Requests/Service/Member/MemberPutRetrieveRequest.php
+++ app/Http/Requests/Service/Member/MemberPutRetrieveRequest.php
@@ -36,8 +36,8 @@ class MemberPutRetrieveRequest extends Request
         // If same before nickname and new nickname
         // Do not check unique nickname in database
         $nicknameRule = Auth::user()->nickname === app('request')->nickname
-            ? "required|max:20|max:3"
-            : "required|availableNickname|max:20|max:3";
+            ? "required|max:20|min:3"
+            : "required|availableNickname|max:20|min:3";
 
         $requiredRule = [
             "nickname" => $nicknameRule,
